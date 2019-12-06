026bf3f58cb18676a84822987db48a47a49d81cb
lhc <15991863734@163.com>
2019-12-06 16:50:29 +0800

    第一次提交
    Signed-off-by: lhc <15991863734@163.com>

---
+++ src/com/xdkj/bean/Person.java
@@ -0,0 +1,17 @@
+package com.xdkj.bean;
+
+import java.util.ArrayList;
+
+/**
+ * @Author：lei
+ * @create $(YEAR)-$(MONTH)-$(DAY) $(TIME)
+ */
+public class Person {
+
+    public static void main(String[] args) {
+        System.out.println("helloword");
+
+
+        }
+    }
+
